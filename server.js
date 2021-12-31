e69a7e7b8bad94f38020b078665b26e4c3da85b2
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-12-31 10:10:55 -0500

    refact to ES6 and add entpoint whoami

--- server.js
+++ server.js
@@ -2,32 +2,41 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
-var express = require('express');
-var app = express();
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/whoami', function (req, res) {
+  console.log(req.ip, req.hostname, req.headers['user-agent'], req.headers['accept-language']);
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
+  });
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
